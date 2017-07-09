7f61bb701aedbde29e4602c0b6485d6641955a25
KATAM VISHNU KOTESWARA REDDY <koteshkv2240@gmail.com>
2017-07-09 16:24:57 +0530

    Add files via upload

---
+++ Ideone.java
@@ -0,0 +1,193 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt;
+		if(a%2==0)
+		System.out.println("The number is even");
+		else
+		System.out.println("The number is odd");
+		
+	}
+}
+
+
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt;
+		int b=s.nextInt;
+		int c=s.nextInt;
+		if(a>b && a>c)
+		System.out.println(a);
+		else if(b>a && b>c)
+		System.out.println(b);
+		else
+			System.out.println(c);
+		
+	}
+}
+
+
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int year=s.nextInt;
+	
+		if(year%4==0)
+		System.out.println("yes");
+	
+		else
+			System.out.println("no");
+		
+	}
+}
+
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	String s="Hello";
+	int i;
+	for(i=0;i<5;i++){
+		System.out.println(s);
+	}
+		
+	
+		
+		
+	}
+}
+
+
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+   Scanner s=new Scanner(System.in);
+   int n=s.nextInt;
+   int i;
+   int sum=0;
+	for(i=1;i<=n;i++){
+		sum=sum+i;
+		
+	}
+			System.out.println(sum);
+	
+			}
+}
+
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		char c = s.next(".").charAt(0);
+		int a=c;
+		if( (a>=97 && a<=122) || (a>=65 && a<=90))
+		System.out.println("yes");
+		else 
+		System.out.println("no");
+		
+	}
+}
+
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+
+		if(a>0)
+		System.out.println("Positve Number");
+		else if(a<0)
+			System.out.println("Negative Number");
+
+                 else
+                      System.out.println("Zero");  
+		
+		
+	}
+}
+
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int a = s.nextInt();
+		String s1=String.valueOf(a);
+		System.out.println(s1.length);
+	}
+}
+
